d5714f54cc143335049a35ccf471fc3cef2afc24
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    Add QS pull down with one finger [1/2]
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5567,6 +5567,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_MODE = "network_traffic_mode";
 
         /**
+         * Allow users to pull down the status bar quickly
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
