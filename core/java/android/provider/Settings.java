3c9714b219304110daf67cf4ef99442d78ba3722
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-08-18 12:27:39 +0530

    [pe-plus]SystemUI: Add visualizer feature
    This is a squash of the following commits:
    
    Author: Utkarsh Gupta <utkarsh.eminem@gmail.com>
    Date:   Tue Aug 18 12:27:39 2015 +0530
    
        SystemUI: use new visualizer from Eleven
    
        Change-Id: I4441440868cac71caa32c91a0875f00a630fe596
        Signed-off-by: Utkarsh Gupta <utkarsh.eminem@gmail.com>
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Mikalacki Sava <mikalackis@gmail.com>
    Date:   Thu Sep 3 23:10:35 2015 +0200
    
        SystemUI: fix NPE crash when Visualizer is not initialized
    
        Change-Id: Ic3774011acfae0312f1642d88c7f5bf2abc9f5a7
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Sep 4 15:55:39 2015 -0700
    
        SystemUI: hide visualizer when keyguard is occluded
    
        Change-Id: Ia943fd829f7a379fee0f657bb08c34b8c6f36caa
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 7 19:27:38 2016 +0100
    
        SystemUI: Make lockscreen visualizer battery friendly
    
        In case of offloaded playback, the device is allowed to sleep.
        However, if the lockscreen visualizer is enabled, the device
        hardly enters deep sleep, even if the screen is off. To prevent
        this, enable the visualizer only when the lockscreen is shown.
    
        Change-Id: I484270694b734c9b53e61a7c0ad74391b54fe8cd
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Mar 17 09:18:56 2016 -0700
    
        SystemUI: more consistent visualizer logic
    
        - only call setVisible from screen on and screen off methods
        - eagerly unregister self to make sure we don't get added twice as a
          listener
        - Add some debugging under the DEBUG flag
    
        Change-Id: Iada13058f87d4c8d8c8b60f6eebf055652ff2c8d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 4 15:29:34 2016 -0700
    
        SystemUI: show visualizer in shade-locked view
    
        This brings back the behavior we had in 12.1 - visualizer would still be
        visible in the shade locked state.
    
        Also with the following improvements:
            * use less alpha
            * always attach/detach in a background thread (Async task)
            * use the statusbar state instead keyguard view state listener
    
        Change-Id: I6b8a57f9f0bba6ba0591de0cb1f94a0eb904cc1c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Dan Pasanen <invisiblek@cyanogenmod.org>
    Date:   Wed May 18 20:27:47 2016 -0500
    
        Visualizer: Let visualizer fill the entire available screen
    
        * This will allow it to be more dynamic (landscape lock screen for instance)
    
        Change-Id: I2ffd6b0ca8832812faaa4cc0ec4cdd6071fbec8c
    
    Author: Matthias Yzusqui <myzb.dev@gmail.com>
    Date:   Wed Sep 21 15:25:00 2016 +0200
    
        SystemUI: fix navbar drawing glitch
    
        Change-Id: I58e255517d35675c043061f2edb398dab0d16ba8
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Dec 2 15:10:28 2017 +0000
    Edit:   Forward-port visualizer feature to O
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Aug 25 01:42:06 2018 +0100
    Edit:   Forward-port visualizer feature to P
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Sep 21 14:56:13 2019 +0100
    Edit:   Forward-port visualizer feature to Q
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Oct 1 10:26:13 20202 +0100
    Edit:   Forward-port visualizer feature to R
    
    Change-Id: I556a233f70f53f08084fd2cca319e3fb05f47243

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9640,6 +9640,13 @@ public final class Settings {
         public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
 
         /**
+         * Whether to show the keyguard visualizer.
+         * Boolean setting. 0 = off, 1 = on.
+         * @hide
+         */
+        public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -210,6 +210,7 @@ import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.SuperStatusBarViewFactory;
 import com.android.systemui.statusbar.SysuiStatusBarStateController;
 import com.android.systemui.statusbar.VibratorHelper;
+import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.statusbar.notification.ActivityLaunchAnimator;
 import com.android.systemui.statusbar.notification.DynamicPrivacyController;
 import com.android.systemui.statusbar.notification.NotificationActivityStarter;
@@ -379,6 +380,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected NotificationShadeWindowView mNotificationShadeWindowView;
     protected StatusBarWindowView mPhoneStatusBarWindow;
     protected PhoneStatusBarView mStatusBarView;
+    private VisualizerView mVisualizerView;
     private int mStatusBarWindowState = WINDOW_STATE_SHOWING;
     protected NotificationShadeWindowController mNotificationShadeWindowController;
     protected StatusBarWindowController mStatusBarWindowController;
@@ -1201,6 +1203,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         });
 
+        mVisualizerView = mNotificationShadeWindowView.findViewById(R.id.visualizerview);
+
         ScrimView scrimBehind = mNotificationShadeWindowView.findViewById(R.id.scrim_behind);
         ScrimView scrimInFront = mNotificationShadeWindowView.findViewById(R.id.scrim_in_front);
         ScrimView scrimForBubble = mBubbleController.getScrimForBubble();
@@ -1552,6 +1556,10 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     public NotificationShadeWindowView getNotificationShadeWindowView() {
         return mNotificationShadeWindowView;
+    }
+
+    public VisualizerView getVisualizerView() {
+        return mVisualizerView;
     }
 
     public StatusBarWindowView getStatusBarWindow() {
@@ -3664,6 +3672,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 || (mDozing && mDozeServiceHost.shouldAnimateScreenOff() && visibleNotOccluded);
 
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
+        mVisualizerView.setDozing(mDozing);
         updateQsExpansionEnabled();
         Trace.endSection();
     }
@@ -3798,6 +3807,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         checkBarModes();
         updateScrimController();
         mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
+        mVisualizerView.setStatusBarState(newState);
         updateKeyguardState();
         Trace.endSection();
     }
@@ -4060,12 +4070,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         @Override
         public void onScreenTurnedOn() {
             mScrimController.onScreenTurnedOn();
+            mVisualizerView.setVisible(true);
         }
 
         @Override
         public void onScreenTurnedOff() {
             mFalsingManager.onScreenOff();
             mScrimController.onScreenTurnedOff();
+            mVisualizerView.setVisible(false);
             if (mNotificationPanelViewController.isQsExpanded()) {
                 mNotificationPanelViewController.closeQs();
             }
