233b8e284aebe804a0d72650dc8e2c858b9477fe
beanstown106 <nbenis106@gmail.com>
2016-01-17 09:14:19 -0500

    Long-press power while display is off for torch
    Fixups for twelve (@neobuddy89)
    Fixups for ten (@sam3000)
    
    Squash of:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Sun Jan 17 09:14:19 2016 -0500
    
        policy: Long-press power while display is off for torch
    
        Long-press the power button while the display is off
        to turn the torchlight on and off.
    
        Credits:
         - Lion0738: The main hooks here:
           https://github.com/lion0738/android_frameworks_base/commit/9af2b7844a4d973c8b6c542d7937f56a24a7e5f1
         - Atlantis: The logic on where to hook into this for power button only
         - Alex Cruz: Helping and giving me some pointers along the way
    
        Change-Id: I14365389990eb06daaa127f5db66df45abf6c064
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Dec 24 13:22:53 2016 -0800
    
        [1/3] Torch long press power: add auto-off function
    
        Change-Id: Icdf50082324f8292859f0df8b271e730b02c84e7
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Fri Dec 3 08:53:59 2021 +0530
    
        Fix long-press power for torch on Android S
    
        * We must define FLAG_IMMUTABLE when creating PendingIntent on Android S
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I49222892c8fbc8a63af580c763e8987b225b11d2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5693,6 +5693,25 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Activate torchlight when power button is
+         * long-pressed while the display is off
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String TORCH_LONG_PRESS_POWER_GESTURE =
+                "torch_long_press_power_gesture";
+
+        /**
+         * When the torch has been turned on by long press on power,
+         * automatically turn off after a configurable number of seconds.
+         * The value is an integer number of seconds in the range 0-3600.
+         * 0 means never automatically turn off.
+         * @hide
+         */
+        public static final String TORCH_LONG_PRESS_POWER_TIMEOUT =
+                "torch_long_press_power_timeout";
+
+        /**
          * Stores the java DateFormat string for the date
          * @hide
          */
@@ -5923,6 +5942,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
             PRIVATE_SETTINGS.add(FP_WAKE_UNLOCK);
             PRIVATE_SETTINGS.add(VOLBTN_MUSIC_CONTROLS);
+            PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_GESTURE);
+            PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_TIMEOUT);
         }
 
         /**
