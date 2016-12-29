e592e6d51af67fd3809880dc3d80e4d4da9537a5
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    base: configure lock screen media artwork blur level [1/2]
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5636,6 +5636,12 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * media artwork wallpaper blur level on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
