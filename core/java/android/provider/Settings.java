b64274aa81d17a53ce568c19f6c4e452d51347d7
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    base: Allow using 4G icon instead LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5206,6 +5206,12 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
 
+	/**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
         /**
          * boolean value. toggles compact layout in navigation bar
          * @hide
