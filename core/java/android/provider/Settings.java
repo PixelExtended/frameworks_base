49194d77c7780825e9b8dd9f57c5495bf60f0421
maxwen <max.weninger@gmail.com>
2017-08-29 18:25:35 +0200

    [1/2] base: double tap to sleep on statusbar
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4795,6 +4795,12 @@ public final class Settings {
          */
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
+	/**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -4957,6 +4963,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
             PRIVATE_SETTINGS.add(VOLTE_ICON_STYLE);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**
