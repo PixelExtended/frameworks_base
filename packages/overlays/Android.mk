cbe5393008d4f84d84512f0bcdef67dff1e364fc
ZeNiXxX <zenixxx.havoc@gmail.com>
2019-11-10 15:26:58 +0100

    base: Add GoogleSans font overlay
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -61,6 +61,7 @@ LOCAL_REQUIRED_MODULES := \
 	DisplayCutoutEmulationTallOverlay \
 	FontArbutusSourceOverlay \
 	FontArvoLatoOverlay \
+	FontGoogleSansOverlay \
 	FontRubikRubikOverlay \
         FontSlateForOnePlusOverlay \
 	IconPackCircularAndroidOverlay \
