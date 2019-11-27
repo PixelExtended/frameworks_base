684d7ca774b7fd2c97674a368d2cbd9c80636795
Marko Man <darkobas@gmail.com>
2019-11-27 23:19:08 +0100

    overlay: Introduce AOSiP Primary
    Change-Id: Icea780d880b5c38598e5d8dff07134fea8e85e4e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -90,6 +90,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
+        PrimaryColorAOSiPBlackOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
