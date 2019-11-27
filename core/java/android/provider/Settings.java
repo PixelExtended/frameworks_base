53d8c2f6257057a966b5dae270ba02dd6ae4e10a
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    Change-Id: I1e636a2647154fdba515f92e0814954758fd3e7e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5591,6 +5591,11 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fingerprint_error_vib";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
