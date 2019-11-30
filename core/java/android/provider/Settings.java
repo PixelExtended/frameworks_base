5442c04d8c94be7259c0225aab03613546eb32bd
SuperDroidBond <superdroidbond@yahoo.com>
2019-11-30 21:18:53 +0530

    VoLTE Icon - User Selectable VoLTE Icon [1/2]
    Idea Taken from https://github.com/Havoc-OS/android_frameworks_base/commit/61ff6b24a01cf92933e2461403dca88b7e255663
    So Thanks to @SKULSHADY & @neobuddy89 for fixes :)
    
    Change-Id: I4d635346c6715d45980696fa28e34e79705eefdf
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4811,7 +4811,13 @@ public final class Settings {
          * Whether to show VoLTE icon or not
          * @hide
          */
-        public static final String SHOW_VOLTE_ICON = "volte_icon";
+        public static final String SHOW_VOLTE_ICON = "show_volte_icon";
+
+        /**
+         * VoLTE icon style
+         * @hide
+         */
+        public static final String VOLTE_ICON_STYLE = "volte_icon_style";
 
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
@@ -4945,6 +4951,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
+            PRIVATE_SETTINGS.add(VOLTE_ICON_STYLE);
         }
 
         /**
