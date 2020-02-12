f8c7e3d0cb9e46e90b06cdb1ec9eac8ad9cd2894
ezio84 <brabus84@gmail.com>
2020-02-12 19:41:28 +0100

    Traffic indicators: don't show two instances in expanded statusbar
    we keep the one on top right of the screen (there we have more space
    and in the future can add more net traffic info)
    
    Change-Id: I098c93b9dd0edd146d08fd23c8c721274b9859ec

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1902,6 +1902,7 @@ public class StatusBar extends SystemUI implements
         if (!isExpanded) {
             mRemoteInputManager.onPanelCollapsed();
         }
+        ((StatusBarIconControllerImpl) mStatusBarIconController).onPanelExpanded(isExpanded);
     }
 
     public ViewGroup getNotificationScrollLayout() {
@@ -4508,6 +4509,7 @@ public class StatusBar extends SystemUI implements
                     updateScrimController();
                     mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
                     updateKeyguardState();
+                    ((StatusBarIconControllerImpl) mStatusBarIconController).setKeyguardShowing(mState == StatusBarState.KEYGUARD);
                     Trace.endSection();
                 }
 
