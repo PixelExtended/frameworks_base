8197f408552577264b16d92a666340aca904b846
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-03-20 21:52:06 +0700

    overlays: add Ancient1 icon shape
    Co-authored-by: kr15714n70 <kristiantoadiwibowo84@gmail.com>
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -47,6 +47,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconShapeCylinderOverlay \
+        IconShapeAncient1Overlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
