8c416d19bd3df6d25aaf339fc7009de3a3b2d6d7
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-03-22 21:33:32 +0700

    overlays: add Ancient5 icon shape
    Co-authored-by: kr15714n70 <kristiantoadiwibowo84@gmail.com>
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -51,6 +51,7 @@ LOCAL_REQUIRED_MODULES := \
         IconShapeAncient2Overlay \
 	IconShapeAncient3Overlay \
 	IconShapeAncient4Overlay \
+        IconShapeAncient5Overlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
