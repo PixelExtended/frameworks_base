1573038962072bea848c4690d11b4bfff89d8a08
El Dainosor <eldainosor@gmail.com>
2020-04-07 23:44:41 +0000

    SystemUI: Add Sammy highlight clock
    It's the same one that it was on oreo/pie but with wallpaper color
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -160,6 +160,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new DividedLinesClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new TypeClockAltController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new SamsungHighlightClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
