f17abebdc1c4e0fcae33edb838a94a0051022805
Aryan Gupta <guptaaryan189@gmail.com>
2020-04-29 10:25:46 +0000

    Add Oneplus Slate Font

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -30,6 +30,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontArbutusSourceOverlay \
 	FontArvoLatoOverlay \
 	FontRubikRubikOverlay \
+        FontSlateForOnePlusOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularPixelLauncherOverlay \
 	IconPackCircularPixelThemePickerOverlay \
