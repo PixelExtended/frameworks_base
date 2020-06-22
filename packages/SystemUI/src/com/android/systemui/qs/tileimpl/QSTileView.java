050ba55ef4bb5f0cbce17b48fbc3a3abc5893e2b
DennySPb <dennyspb@gmail.com>
2020-06-22 17:45:22 +0000

    QSTile: tint second line of active tile as well
    also make it optional

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
@@ -146,6 +146,11 @@ public class QSTileView extends QSTileBaseView {
             mLabel.setText(state.label);
         }
         if (!Objects.equals(mSecondLine.getText(), state.secondaryLabel)) {
+          if (state.state == Tile.STATE_ACTIVE && mTintEnabled) {
+                mSecondLine.setTextColor(mColorLabelActive);
+            } else if (state.state == Tile.STATE_INACTIVE || !mTintEnabled) {
+                mSecondLine.setTextColor(mColorLabelDefault);
+            }
             mSecondLine.setText(state.secondaryLabel);
             mSecondLine.setVisibility(TextUtils.isEmpty(state.secondaryLabel) ? View.GONE
                     : View.VISIBLE);
