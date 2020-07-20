6cbd5faff0e951c6bd0741cd7c44be25448b5dc9
Aryan Gupta <guptaaryan189@gmail.com>
2020-07-20 15:51:53 +0000

    Restore AOSP font styling for traffic

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -397,7 +397,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
 
      protected void setSpacingAndFonts() {
         setCompoundDrawablePadding(txtImgPadding);
-        setTypeface(Typeface.create("google-sans", Typeface.NORMAL));
+        setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
         setLineSpacing(0.75f, 0.75f);
     }
 
