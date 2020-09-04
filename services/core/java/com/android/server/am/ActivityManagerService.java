147d6aaf1a83047155bbdd4ec88cabd2966e472d
Christian Oder <myself5@carbonrom.org>
2020-09-04 23:30:39 +0200

    fwb: Rework Typeface handling to only refresh theme when necessary
    Set font at boot completed once and only when changed afterwards, don't
    hardcode true to always refresh theme on Zygote start.
    
    This improve app startup times due to not having to regenerate default Typeface
    and solves app starting issues caused by Typeface changes running OOM.
    
    Also rework the way we send the information to Zygote to also allow clearing
    the USAP pool on change.
    
    Change-Id: I80a92e4fac4a134f48919b7e8a9bcceb528ebc72

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -575,6 +575,8 @@ public class ActivityManagerService extends IActivityManager.Stub
     private static final int NATIVE_DUMP_TIMEOUT_MS = 2000; // 2 seconds;
     private static final int JAVA_DUMP_MINIMUM_SIZE = 100; // 100 bytes.
 
+    private static final String PROP_REFRESH_TYPEFACE = "sys.refresh_typeface";
+
     OomAdjuster mOomAdjuster;
     final LowMemDetector mLowMemDetector;
 
@@ -1670,6 +1672,8 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private final PlatformCompat mPlatformCompat;
 
+    final ZygoteTypefaceRefreshUpdate mZygoteTypefaceRefresh;
+
     PackageManagerInternal mPackageManagerInt;
     PermissionManagerServiceInternal mPermissionManagerInt;
 
@@ -2519,6 +2523,41 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
         return mAppOpsManager;
     }
+    
+    static class ZygoteTypefaceRefreshUpdate extends ContentObserver {
+
+        private final Context mContext;
+
+        public ZygoteTypefaceRefreshUpdate(Handler handler, Context context) {
+            super(handler);
+            mContext = context;
+        }
+
+        public void registerObserver() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.FONT_SCALE),
+                    false,
+                    this);
+            update();
+        }
+
+        private void update() {
+            // Check if zygote should refresh its fonts
+            if (SystemProperties.getBoolean(PROP_REFRESH_TYPEFACE, false)) {
+                SystemProperties.set(PROP_REFRESH_TYPEFACE, "false");
+                ZYGOTE_PROCESS.refreshTypeface();
+            }
+        }
+
+        public void onChange(boolean selfChange) {
+            update();
+        }
+    }
+
+    @VisibleForTesting
+    public ActivityManagerService(Injector injector) {
+        this(injector, null /* handlerThread */);
+    }
 
     /**
      * Provides the basic functionality for activity task related tests when a handler thread is
@@ -2563,6 +2602,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mProcStartHandlerThread = null;
         mProcStartHandler = null;
         mHiddenApiBlacklist = null;
+        mZygoteTypefaceRefresh = null;
         mFactoryTest = FACTORY_TEST_OFF;
         mUgmInternal = LocalServices.getService(UriGrantsManagerInternal.class);
         mInternal = new LocalService();
@@ -2706,6 +2746,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         };
 
         mHiddenApiBlacklist = new HiddenApiSettings(mHandler, mContext);
+        mZygoteTypefaceRefresh = new ZygoteTypefaceRefreshUpdate(mHandler, mContext);
 
         Watchdog.getInstance().addMonitor(this);
         Watchdog.getInstance().addThread(mHandler);
@@ -9505,6 +9546,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         final long waitForNetworkTimeoutMs = Settings.Global.getLong(resolver,
                 NETWORK_ACCESS_TIMEOUT_MS, NETWORK_ACCESS_TIMEOUT_DEFAULT_MS);
         mHiddenApiBlacklist.registerObserver();
+        mZygoteTypefaceRefresh.registerObserver();
 
         final long pssDeferralMs = DeviceConfig.getLong(DeviceConfig.NAMESPACE_ACTIVITY_MANAGER,
                 ACTIVITY_START_PSS_DEFER_CONFIG, 0L);
