d8bdf39f24567e71824b9e1c42908551ddfd381e
El Dainosor <eldainosor@gmail.com>
2020-10-11 13:21:19 -0400

    base: Add leaf icon shape

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -55,6 +55,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedThemePickerOverlay \
 	IconShapePebbleOverlay \
 	IconShapeHeartOverlay \
+	IconShapeLeafOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
