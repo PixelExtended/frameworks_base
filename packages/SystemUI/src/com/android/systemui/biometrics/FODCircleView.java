3456f914c8b51772e594fe8e7bc6a1c70b4ddbb7
daniml3 <danimoral1001@gmail.com>
2020-11-24 22:11:20 +0100

    FODCircleView: changes to FOD circle and animation visibility
    FOD Animation:
    * Hide the FOD animation along with the circle, this should avoid that the animation keeps visible and frozen after unlocking with FOD animations enabled.
    * Show the FOD animation along with the circle, so it is also shown with the screen off FOD.
    
    FOD Circle:
    * Show the circle even if the fade is in progress, so FOD works with screen off in one tap.
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Iae0a0fc37628a1eed8209d818cb30a1de1f58f30
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -466,11 +466,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
-            mHandler.post(() -> mFODAnimation.showFODanimation());
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             hideCircle();
-            mHandler.post(() -> mFODAnimation.hideFODanimation());
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
             return true;
@@ -549,6 +547,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         setDim(true);
         dispatchPress();
 
+        mHandler.post(() -> mFODAnimation.showFODanimation());
+
         setImageDrawable(null);
         invalidate();
     }
@@ -561,6 +561,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         dispatchRelease();
         setDim(false);
+
+        mHandler.post(() -> mFODAnimation.hideFODanimation());
 
         setKeepScreenOn(false);
     }
