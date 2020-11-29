10f2cd5a5606dea804f094984f46c6fc11d6c0e0
darkbeast69 <guptaaryan189@gmail.com>
2020-11-29 14:22:43 +0000

    fixup! extend flashlight api to SystemUIGoogle
    Signed-off-by: darkbeast69 <guptaaryan189@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -82,6 +82,7 @@ import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
@@ -171,6 +172,7 @@ public class StatusBarGoogle extends StatusBar {
             PluginManager pluginManager,
             Optional<Divider> dividerOptional,
             LightsOutNotifController lightsOutNotifController,
+            FlashlightController flashlightController,
             StatusBarNotificationActivityStarter.Builder
                     statusBarNotificationActivityStarterBuilder,
             ShadeController shadeController,
@@ -249,6 +251,7 @@ public class StatusBarGoogle extends StatusBar {
                 pluginManager,
                 dividerOptional,
                 lightsOutNotifController,
+                flashlightController,
                 statusBarNotificationActivityStarterBuilder,
                 shadeController,
                 superStatusBarViewFactory,
