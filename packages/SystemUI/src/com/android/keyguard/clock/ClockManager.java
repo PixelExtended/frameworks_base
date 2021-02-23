61cd8c744beeb64ee78dc2e394c4f5039f2d90b0
AndroBuddy <anirudhchimpidi8@gmail.com>
2021-02-23 16:03:39 +0530

    Introduce Fluid lock clock V2
    From V1:
    AshutoshSundresh:
    -Move string to controller instead of xml as it isn't translatable anyways
    -Port to A11
    -Prevent it from getting stuck on Ambient Display
    
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -167,6 +167,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new Fluidv2ClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
