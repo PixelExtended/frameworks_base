3f7e7592fa901cafd6b47527551ac115672997c6
Aryan Gupta <guptaaryan189@gmail.com>
2021-04-05 05:42:46 +0000

    Clocks: Clean User toggle to show/hide status area

--- packages/SystemUI/src/com/android/keyguard/clock/SamsungHighlightClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SamsungHighlightClockController.java
@@ -211,7 +211,6 @@ public class SamsungHighlightClockController implements ClockPlugin {
 
     @Override
     public boolean shouldShowStatusArea() {
-        if (mContext == null) return true;
-        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_SHOW_STATUS_AREA, 1) == 1;
+         return true;
     }
 }
