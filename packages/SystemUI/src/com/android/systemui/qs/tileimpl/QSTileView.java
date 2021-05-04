95ee4c9cebe219e1180131be372707f087e13160
DennySPb <dennyspb@gmail.com>
2021-05-04 14:57:48 +0300

    QSTint: Tint secondary line and expanded indicator as well
    * This make accent color tinting more solid
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I32ec82fbde7fdeab939deb48d9e611067542776d
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
@@ -47,7 +47,7 @@ public class QSTileView extends QSTileBaseView {
     private ImageView mPadLock;
     private int mState;
     private ViewGroup mLabelContainer;
-    private View mExpandIndicator;
+    private ImageView mExpandIndicator;
     private View mExpandSpace;
     private ColorStateList mColorLabelDefault;
     private ColorStateList mColorLabelActive;
@@ -140,8 +140,12 @@ public class QSTileView extends QSTileBaseView {
         if (setQsUseNewTint == 1) {
             if (state.state == Tile.STATE_ACTIVE) {
                 mLabel.setTextColor(mColorLabelActive);
+                mExpandIndicator.setImageTintList(mColorLabelActive);
+                mSecondLine.setTextColor(mColorLabelActive);
             } else if (state.state == Tile.STATE_INACTIVE) {
                 mLabel.setTextColor(mColorLabelDefault);
+                mExpandIndicator.setImageTintList(mColorLabelDefault);
+                mSecondLine.setTextColor(mColorLabelDefault);
             }
         }
         boolean dualTarget = DUAL_TARGET_ALLOWED && state.dualTarget;
