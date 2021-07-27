a69c57d055d46799d58dc26bd17a5057f4cd986d
romanbb <romanbirg@gmail.com>
2021-07-27 16:07:36 +0000

    Utils: Add isPackageInstalled boolean

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -105,6 +105,25 @@ public class CustomUtils {
             }
         }
 
+   public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
    // Switch to last app
     public static void switchToLastApp(Context context) {
         final ActivityManager am =
