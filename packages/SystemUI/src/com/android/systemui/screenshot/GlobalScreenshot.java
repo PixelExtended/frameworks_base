8432f541712fbf42b2dd7aea0915c7283c7961f6
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-09-12 16:34:37 +0000

    SystemUI: Unblock gestural navigation on clearScreenshot()
    If user powers screen off when partial screenshot UI is visible,
    it will hide the UI but leave gestural navigation disabled.
    Also the partial screenshot UI would be visible when taking a
    new (normal) screenshot.
    
    Change-Id: Ie8138bb0b771ec0e21ceb48e52872c05c946d3b4

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -429,7 +429,9 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
     void hideScreenshotSelector() {
         setLockedScreenOrientation(false);
-        mWindowManager.removeView(mScreenshotLayout);
+        if (mScreenshotLayout.getWindowToken() != null) {
+            mWindowManager.removeView(mScreenshotLayout);
+        }
         mScreenshotSelectorView.stopSelection();
         mScreenshotSelectorView.setVisibility(View.GONE);
         mCaptureButton.setVisibility(View.GONE);
@@ -883,6 +885,7 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         mActionsContainer.setTranslationY(0);
         mActionsContainerBackground.setTranslationY(0);
         mScreenshotPreview.setTranslationY(0);
+        hideScreenshotSelector();
     }
 
     /**
