9f7862142a58ddb2ca42c5c417adc8a16bc54c8a
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: allow hiding navbar [1/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5432,6 +5432,13 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * Whether to hide navbar pill and keyboard space.
+         * Default 0.
+         * @hide
+         */
+        public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
