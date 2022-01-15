48eaacf79c5fcfbdc48a9e9da8178c9df433c861
Pranav Vashi <neobuddy89@gmail.com>
2022-01-15 17:09:09 +0530

    SystemUI: Refactor colored statusbar icons
    * Remove painful systemui checks everywhere in StatusBariconView
    * Apply NO_COLOR when colored icons are enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -393,6 +393,8 @@ public interface StatusBarIconController {
         protected DemoStatusIcons mDemoStatusIcons;
 
         protected ArrayList<String> mBlockList = new ArrayList<>();
+
+        private final boolean mNewIconStyle;
         private final boolean mShowNotificationCount;
 
         private boolean mShowWifiStandard;
@@ -414,6 +416,8 @@ public interface StatusBarIconController {
             mIconSize = mContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.status_bar_icon_size);
             mLocation = location;
+ 	    mNewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
             mShowNotificationCount = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.STATUSBAR_NOTIF_COUNT,
                 mContext.getResources().getBoolean(R.bool.config_statusBarShowNumber) ? 1 : 0,
@@ -508,6 +512,7 @@ public interface StatusBarIconController {
         protected StatusBarIconView addIcon(int index, String slot, boolean blocked,
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
+            view.setIconStyle(mNewIconStyle);
             view.setShowCount(mShowNotificationCount);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
