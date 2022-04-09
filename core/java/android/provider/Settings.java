ddb830a516cf8cd7f49c2aac16033a1d5179052c
jhenrique09 <jhsv09@gmail.com>
2022-04-09 00:43:29 +0000

    Add TETHERING_ALLOW_VPN_UPSTREAMS setting
    Change-Id: I8b388dbb356567de25457bb98ae8c64cd0ccb6a1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10976,6 +10976,13 @@ public final class Settings {
         public static final String SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS = "show_combined_status_bar_signal_icons";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         * 0 = false, 1 = true
+         */
+        @SuppressLint("NoSettingsProvider")
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
