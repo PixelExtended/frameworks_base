943add14693b923ea4eeb5e46610e98548cb6a26
ProchyGaming <dominikprochazka100@gmail.com>
2022-04-21 23:37:31 +0200

    VolumePanel: Add long click for expand icon
    Change-Id: I4986ae91a4213d55fe9e5cead687e089c14b7919
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -49,6 +49,7 @@ import android.app.Dialog;
 import android.app.KeyguardManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothProfile;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -171,6 +172,9 @@ public class VolumeDialogImpl implements VolumeDialog,
     private static final String TYPE_DISMISS = "dismiss";
     /** Volume dialog slider animation. */
     private static final String TYPE_UPDATE = "update";
+
+    private static final ComponentName SOUND_SETTING_COMPONENT = new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$SoundSettingsActivity");
 
     private final int mDialogShowAnimationDurationMs;
     private final int mDialogHideAnimationDurationMs;
@@ -1313,6 +1317,16 @@ public class VolumeDialogImpl implements VolumeDialog,
                 Dependency.get(MediaOutputDialogFactory.class).dismiss();
                 Dependency.get(ActivityStarter.class).startActivity(intent,
                         true /* dismissShade */);
+            });
+            mSettingsIcon.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Intent intent = new Intent().setComponent(SOUND_SETTING_COMPONENT);
+                    mMediaOutputDialogFactory.dismiss();
+                    dismissH(DISMISS_REASON_SETTINGS_CLICKED);
+                    mActivityStarter.startActivity(intent, true /* dismissShade */);
+                    return true;
+                }
             });
         }
     }
