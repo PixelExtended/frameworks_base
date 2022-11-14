e993f0febaafe54e1a8f940ba8b982dba245b74f
beanstown106 <nbensis106@gmail.com>
2022-11-14 20:21:02 +0000

    Fingerprint authentication vibration [1/2]
    Change-Id: I443dd4eaf49c9756cd78ae5daac462d5eb7f714f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6039,6 +6039,18 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FP_SUCCESS_VIBRATE = "fingerprint_success_vib";
+
+        /**
+         * Whether to enable or disable vibration on failed fingerprint auth
+         * @hide
+         */
+        public static final String FP_ERROR_VIBRATE = "fingerprint_error_vib";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
