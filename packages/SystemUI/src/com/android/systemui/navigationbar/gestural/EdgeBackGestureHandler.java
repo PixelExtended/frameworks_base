a70796fe19271062c572b1f63d1781af352cb04a
DennySPb <dennyspb@gmail.com>
2023-03-26 14:07:03 +0000

    SystemUI: Optional haptic feedback on back gesture [1/2] Change-Id: I3bd949cde08ac9a0513e05f2356e84fde07ed767 Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If70a4d639a80272b8e4f3dc2018ec7395696feab

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -35,11 +35,14 @@ import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.hardware.input.InputManager;
+import android.os.AsyncTask;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Trace;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
 import android.provider.DeviceConfig;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
@@ -264,6 +267,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     private float mLongSwipeWidth;
 
     private int mEdgeHeight;
+    private boolean mEdgeHapticEnabled;
+    private final Vibrator mVibrator;
 
     // For Tf-Lite model.
     private BackGestureTfClassifierProvider mBackGestureTfClassifierProvider;
@@ -287,6 +292,10 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             new NavigationEdgeBackPlugin.BackCallback() {
                 @Override
                 public void triggerBack(boolean isLongPress) {
+                    if (mEdgeHapticEnabled) {
+                        vibrateBack(true /* Click */);
+                    }
+
                     // Notify FalsingManager that an intentional gesture has occurred.
                     // TODO(b/186519446): use a different method than isFalseTouch
                     mFalsingManager.isFalseTouch(BACK_GESTURE);
@@ -372,6 +381,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             Provider<BackGestureTfClassifierProvider> backGestureTfClassifierProviderProvider,
             FeatureFlags featureFlags) {
         mContext = context;
+        mVibrator = context.getSystemService(Vibrator.class);
         mDisplayId = context.getDisplayId();
         mMainExecutor = executor;
         mBackgroundExecutor = backgroundExecutor;
@@ -457,6 +467,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         Resources res = mNavigationModeController.getCurrentUserContext().getResources();
         mEdgeWidthLeft = mGestureNavigationSettingsObserver.getLeftSensitivity(res);
         mEdgeWidthRight = mGestureNavigationSettingsObserver.getRightSensitivity(res);
+        mEdgeHapticEnabled = mGestureNavigationSettingsObserver.getEdgeHaptic();
         mIsBackGestureAllowed =
                 !mGestureNavigationSettingsObserver.areNavigationButtonForcedVisible();
 
@@ -537,6 +548,12 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
 
     public void onSettingsChanged() {
         updateEdgeHeightValue();
+    }
+
+    private void vibrateBack(boolean light) {
+            AsyncTask.execute(() ->
+                    mVibrator.vibrate(VibrationEffect.get(light ? VibrationEffect.EFFECT_CLICK :
+                        VibrationEffect.EFFECT_HEAVY_CLICK, true  /* fallback */)));
     }
 
     public void onNavBarTransientStateChanged(boolean isTransient) {
