c9b395d8a5a1a7538b7cee3a350e0a300c5dab77
DennySPb <dennyspb@gmail.com>
2023-03-26 14:07:03 +0000

    SystemUI: Optional haptic feedback on back gesture [1/2] Change-Id: I3bd949cde08ac9a0513e05f2356e84fde07ed767 Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: If70a4d639a80272b8e4f3dc2018ec7395696feab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6081,6 +6081,9 @@ public final class Settings {
          */
         public static final String OMNI_CUSTOM_FP_ICON = "custom_fingerprint_icon";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6263,6 +6266,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_ENABLED);
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_TIMEOUT);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
+	    PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
         }
         /**
          * These entries are considered common between the personal and the managed profile,
